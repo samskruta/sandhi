ff15ecbf4337cb2e3c7140320fc15ee8d3be8b0c
ganarajpr <ganarajpr@gmail.com>
2021-12-19 22:15:10 +0000

    add csv tests

--- src/lib/apply-sandhi.js
+++ src/lib/apply-sandhi.js
@@ -1,6 +1,6 @@
-
+import rules from "./sandhi-rules";
 const applySandhi = () => {
-
+  return '';
 };
   
   export default applySandhi;
--- src/lib/tests/test.js
+++ src/lib/tests/test.js
@@ -1,5 +1,23 @@
 import applySandhi from '../index';
+import fs from 'fs';
+import { parse } from 'csv/dist/umd/sync';
 
-test('Runs without crashing', () => {
-  applySandhi();
+
+const contents = fs.readFileSync('./src/lib/tests/asht.csv','utf8');
+const rows = parse(contents, {
+  columns: true,
+  skip_empty_lines: true
 });
+
+describe('Rule Based Corpus - Internal', () => {
+
+  rows.forEach(row => {
+    const split = row.Split.split('+');
+    test(`it works for ${row.Word}`, () => {
+      const combined = applySandhi(...split);
+      expect(combined).toEqual(row.Word);
+    });
+  });
+  
+});
+
